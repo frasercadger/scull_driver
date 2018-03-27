35424e6661935ec5e4b2c7a2c797fd9ed8dfc115
Fraser Cadger <frasercadger@gmail.com>
2018-03-27 19:09:04 +0100

    Add code for dynamic registering/unregistering of major and minor device numbers

--- scull.c
+++ scull.c
@@ -18,23 +18,64 @@
     driver.
 */
 
+/*
+ * Copyright (C) 2001 Alessandro Rubini and Jonathan Corbet
+ * Copyright (C) 2001 O'Reilly & Associates
+ *
+ * The source code in this file can be freely used, adapted,
+ * and redistributed in source or binary form, so long as an
+ * acknowledgment appears in derived source files.  The citation
+ * should list that the code comes from the book "Linux Device
+ * Drivers" by Alessandro Rubini and Jonathan Corbet, published
+ * by O'Reilly & Associates.   No warranty is attached;
+ * we cannot take responsibility for errors or fitness for use.
+ *
+ */
+
 /* Includes */
+#include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/types.h>
 
-/* Function prototypes */
+/* Local constants */
+static const char *scull_name = "scull";
+/* Currently we're only supporting scull0-scull3 */
+static const unsigned int scull_device_count = 4;
+
+/* Static globals */
+static int scull_major;
+static int scull_minor = 0;
+
+/* TODO: Function prototypes */
 
 /* Function definitions */
 
 static int __init scull_init(void)
 {
-	/* TODO: Add init code */
+	int retval;
+	dev_t dev;
+
+	/* Get major and minor numbers via dynamic allocation */
+	retval = alloc_chrdev_region(&dev, scull_minor,
+				     scull_device_count, scull_name);
+	if(retval < 0)
+	{
+		/* Something went wrong */
+		printk(KERN_ERR "Dynamic allocation of major number error\n");
+	}
+
+	scull_major = MAJOR(dev);
+
 	return 0;
 }
 module_init(scull_init);
 
 static void __exit scull_exit(void)
 {
-	/* TODO: Add exit code */
+	dev_t devno = MKDEV(scull_major, scull_minor);
+
+	/* Unregister device number allocation */
+	unregister_chrdev_region(devno, scull_device_count);
 }
 module_exit(scull_exit);
