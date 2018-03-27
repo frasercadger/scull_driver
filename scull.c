23d65c7b33ca3896c58e6e0df30a259f2e75cfa8
Fraser Cadger <frasercadger@gmail.com>
2018-03-27 18:36:33 +0100

    Add file header and some comments to scull.c
    Note, I've not included the header from the LDD example code yet, as at the moment I haven't
    included any of their code, but when I'll add it as soon as I'm actually using their code.

--- scull.c
+++ scull.c
@@ -1,5 +1,30 @@
+/*
+    Copyright: Fraser Cadger (2018) <frasercadger@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Description: Simple Character Utiltity for Loading Localities (scull)
+    driver.
+*/
+
+/* Includes */
 #include <linux/init.h>
 #include <linux/module.h>
+
+/* Function prototypes */
+
+/* Function definitions */
 
 static int __init scull_init(void)
 {
