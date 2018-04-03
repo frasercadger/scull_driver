cf52032bb8fbeb910205236dc1485c4634efaaa5
Fraser Cadger <frasercadger@gmail.com>
2018-04-03 13:45:57 +0100

    Change scull.h scull_read() prototype last parameter to f_pos not f_post

--- scull.h
+++ scull.h
@@ -61,7 +61,7 @@ struct scull_dev {
 int scull_open(struct inode *inode, struct file *filp);
 int scull_release(struct inode *inode, struct file *filp);
 ssize_t scull_read(struct file *filp, char __user *buf, size_t count,
-		   loff_t *f_post);
+		   loff_t *f_pos);
 ssize_t scull_write(struct file *filp, const char __user *buf, size_t count,
 		   loff_t *f_pos);
 
